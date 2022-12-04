0abeb15d1e7a8c9510266bb6a859e16a12935926
alecchangod <alec3d02@gmail.com>
2022-12-04 14:25:54 +0800

    monet: config EXTRA_UDFPS_ANIMATIONS

--- device.mk
+++ device.mk
@@ -55,18 +55,5 @@ TARGET_HAS_FOD := true
 # Properties
 -include $(LOCAL_PATH)/properties.mk
 
-<<<<<<< HEAD
-# Inherit from sm7250-common
-$(call inherit-product, device/xiaomi/sm7250-common/lito.mk)
-
-# Don't build debug for host or device
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := true
-ART_BUILD_TARGET_NDEBUG := true
-ART_BUILD_TARGET_DEBUG := true
-ART_BUILD_HOST_NDEBUG := true
-ART_BUILD_HOST_DEBUG := false
-USE_DEX2OAT_DEBUG := true
-=======
 # UDFPS animations
 EXTRA_UDFPS_ANIMATIONS := true
->>>>>>> 9bf443e (added Call recording for Google Dialer)
