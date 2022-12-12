fea36da1d7bb3f4a8801a264257529bdf9debeeb
alecchangod <alec3d02@gmail.com>
2022-12-12 07:40:23 +0000

    inital commit for calyxOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/xtended_monet.mk
+   $(LOCAL_DIR)/calyx_monet.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_monet-user \
-    xtended_monet-userdebug \
-    xtended_monet-eng
+    calyx_monet-user \
+    calyx_monet-userdebug \
+    calyx_monet-eng
