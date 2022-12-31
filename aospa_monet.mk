b56144c90b36324ce8a919e2a75d715a1cd2eb36
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-01-01 03:34:09 +0800

    lineage_monet ==> aospa_monet

--- aospa_monet.mk
+++ aospa_monet.mk
@@ -8,18 +8,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
 # Inherit from monet device
 $(call inherit-product, device/xiaomi/monet/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_monet
+PRODUCT_NAME := aospa_monet
 PRODUCT_DEVICE := monet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 10 Lite 5G
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="monet-user 12 SKQ1.211006.001 V13.0.2.0.SJIEUXM release-keys"
