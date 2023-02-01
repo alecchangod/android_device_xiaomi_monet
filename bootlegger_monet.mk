b85c6fcd62b770060215b2275ba7043355044dd0
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-02-01 20:29:11 +0800

    monet: bootlegger additions

--- bootlegger_monet.mk
+++ bootlegger_monet.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/monet/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := xtended_monet
+PRODUCT_NAME := bootlegger_monet
 PRODUCT_DEVICE := monet
 BOARD_VENDOR := Xiaomi
 TARGET_VENDOR := Xiaomi
@@ -25,15 +25,17 @@ PRODUCT_MODEL := M2002J9G
 
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
+## Bootleggers ROM additions
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+TARGET_USE_SINGLE_BOOTANIMATION := true
+TARGET_PICK_BOOTANIMATION := "6"
 
-# Xtended Stuffs
-XTENDED_BUILD_MAINTAINER := alecchangod
-XTENDED_BUILD_TYPE := OFFICIAL
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_BLUR := true
-XTENDED_BUILD_DONATE_URL := https://www.paypal.com/paypalme/Alec174
+# Maintainer Prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bootleggers.maintainer=alecchangod
+TARGET_BOOTLEG_ARCH := arm64
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="monet-user 12 SKQ1.211006.001 V13.0.2.0.SJIEUXM release-keys"
