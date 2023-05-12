b08c4dfd55407375733158f8be3aad5c88523929
Alec Chan <alec3d02@gmail.com>
2023-05-13 01:28:12 +0800

    initial voidUI

--- aosp_monet.mk
+++ aosp_monet.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common Voui stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from monet device
 $(call inherit-product, device/xiaomi/monet/device.mk)
@@ -18,10 +18,10 @@ $(call inherit-product, device/xiaomi/monet/device.mk)
 $(call inherit-product, vendor/xiaomi-firmware/monet/firmware.mk)
 
 # MIUI Camera
-$(call inherit-product, vendor/xiaomi/camera/products/miuicamera.mk)
+#$(call inherit-product, vendor/xiaomi/camera/products/miuicamera.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := xtended_monet
+PRODUCT_NAME := aosp_monet
 PRODUCT_DEVICE := monet
 BOARD_VENDOR := Xiaomi
 TARGET_VENDOR := Xiaomi
@@ -31,14 +31,17 @@ PRODUCT_MODEL := M2002J9G
 
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
-
-# Xtended Stuffs
-XTENDED_BUILD_MAINTAINER := alecchangod
-XTENDED_BUILD_TYPE := OFFICIAL
+# Rom Specific Flags
+TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-XTENDED_BUILD_DONATE_URL := https://www.paypal.com/paypalme/Alec174
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+
+# VoidUI Maintainer Flags
+VOID_MAINTAINER := alecchangod
+CUSTOM_BUILD_TYPE := UNOFFICIAL
+
 
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
