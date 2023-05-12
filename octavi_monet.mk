53b3bfdcf2f9e518d872dbd903076ca42d1ad9ed
Alec Chan <alec3d02@gmail.com>
2023-05-12 23:05:28 +0800

    OctaviOS bringup

--- octavi_monet.mk
+++ octavi_monet.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common Octavi stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
 # Inherit from monet device
 $(call inherit-product, device/xiaomi/monet/device.mk)
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/xiaomi-firmware/monet/firmware.mk)
 $(call inherit-product, vendor/xiaomi/camera/products/miuicamera.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := xtended_monet
+PRODUCT_NAME := octavi_monet
 PRODUCT_DEVICE := monet
 BOARD_VENDOR := Xiaomi
 TARGET_VENDOR := Xiaomi
@@ -30,15 +30,11 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2002J9G
 
 TARGET_INCLUDE_PIXEL_CHARGER := true
-
-
-# Xtended Stuffs
-XTENDED_BUILD_MAINTAINER := alecchangod
-XTENDED_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
-XTENDED_BUILD_DONATE_URL := https://www.paypal.com/paypalme/Alec174
+TARGET_WANTS_FOD_ANIMATIONS := true
+OCTAVI_DEVICE_MAINTAINER := alecchangod
 
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
