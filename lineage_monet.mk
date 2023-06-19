9045ab1a08b96cc4a3bb5cb4c9fe37ad01bc7576
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-06-19 11:04:41 +0800

    monet: initial alpha droid

--- lineage_monet.mk
+++ lineage_monet.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common yaap stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common alpha droid stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from monet device
 $(call inherit-product, device/xiaomi/monet/device.mk)
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/xiaomi-firmware/monet/firmware.mk)
 #$(call inherit-product, vendor/xiaomi/camera/products/miuicamera.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := yaap_monet
+PRODUCT_NAME := lineage_monet
 PRODUCT_DEVICE := monet
 BOARD_VENDOR := Xiaomi
 TARGET_VENDOR := Xiaomi
@@ -30,11 +30,28 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2002J9G
 
 TARGET_INCLUDE_PIXEL_CHARGER := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_WANTS_FOD_ANIMATIONS := true
-OCTAVI_DEVICE_MAINTAINER := alecchangod
+
+# GAPPS flag
+# WITH_GAPPS := true
+# TARGET_CORE_GAPPS := true
+
+# Alpha prop
+ALPHA_BUILD_TYPE := UNOFFICIAL
+ALPHA_MAINTAINER := alecchangod
+
+# Alpha device prop
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_MATLOG := true
+TARGET_USE_GRAPHENE_CAMERA := true
+TARGET_USE_PIXEL_LAUNCHER := false
+TARGET_EXCLUDES_AUDIOFX := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Bootanimation prop
+USE_LEGACY_BOOTANIMATION := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
